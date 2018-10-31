a1aa726bee8b4a0d3e8a41bcb580a4591f77f56a
Minjae Joe <ussr1285@gmail.com>
2018-10-31 14:37:03 +0900

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -34,23 +34,44 @@ public class Main {
         }
         br.close();
         total_text = temp_total_text.toString();
+
         for (int i=0; i<amount_problem; i++) {
             index_num[i] = total_text.indexOf(Integer.toString(i + 1) + ".");
+            //System.out.println("-------- "+i+"st's index is aright. -----------");
         }
-     //   System.out.println("first end");
+        //   System.out.println("first end");
         for (int i=0; i<amount_problem; i++){
             if(i < amount_problem-1){
+                //System.out.println(index_num[i]+" "+index_num[i+1]);
                 temp_string[i] = total_text.substring(index_num[i], index_num[i+1]);
-        }
+            }
             else{
+                // System.out.println(index_num[i]);
                 temp_string[i] = total_text.substring(index_num[i]);
             }
+            //System.out.println(temp_string[i]);
             index_select[i][0] = temp_string[i].indexOf("①");
             index_select[i][1] = temp_string[i].indexOf("②");
             index_select[i][2] = temp_string[i].indexOf("③");
             index_select[i][3] = temp_string[i].indexOf("④");
             index_select[i][4] = temp_string[i].indexOf("⑤");
 
+            /*
+            int temp_index_dot;
+            int question_mark = temp_string[i].substring(3).indexOf("?");
+            int dot_mark = temp_string[i].substring(3).indexOf(".");
+
+            if (dot_mark < question_mark){
+                temp_index_dot = dot_mark;
+                //System.out.println("---------d---"+dot_mark);
+                //System.out.println("------------"+question_mark);
+            }
+            else{
+                temp_index_dot = question_mark;
+                //System.out.println("---------q---"+question_mark);
+                //System.out.println("------------"+dot_mark);
+            }
+            */
             int temp_index_dot = temp_string[i].substring(3).indexOf(".");
             if (temp_index_dot != -1){
                 index_question_dot[i] = temp_index_dot+3; // 바로 위의 변수 초기화에서 2부터 시작하는 문장에서의 .을 찾으므로 +2를 해주어야 원래 위치.
@@ -58,25 +79,31 @@ public class Main {
 
         }
 
-    //    System.out.println("second end");
+        //    System.out.println("second end");
         for(int i=0; i<amount_problem; i++){
+            System.out.println("--------- "+i+"st is problem?! ---------");
+            System.out.println("---------"+index_question_dot[i]+"--"+index_select[i][0]);
+
             problem[i].get_question(temp_string[i].substring(3, index_question_dot[i]+1)); //점 포함을 위해 +1
             problem[i].get_example(temp_string[i].substring(index_question_dot[i]+1, index_select[i][0])); //점 제외를 위해 +1
             for (int j = 0; j < 5; j++){
                 if(j < 4){ // 5 - 1 = 4
+                    //System.out.println("----------------"+index_select[i][j]);
                     problem[i].get_select(j, temp_string[i].substring(index_select[i][j]+0, index_select[i][j+1])); //번호 제외를 위해 +1
                 }
                 else{
                     problem[i].get_select(j, temp_string[i].substring(index_select[i][j]+0)); //동일
                 }
             }
+            System.out.println("-------- "+i+"st is not problem. -----------");
         }
         for (int i=0; i<amount_problem;i++) {
             problem[i].print_info();
+            problem[i].print_file();
         }
 
         System.out.println("분리완료");
-    //    System.out.println("fourth end");
+        //    System.out.println("fourth end");
 
         System.out.println("데이터베이스의 정보를 입력해주십시오.( ex.localhost:3306/db ) :"); //db
         String target_db = sc.next(); //"localhost:3306/db";
@@ -105,6 +132,3 @@ public class Main {
     }
 
 }
-
-
-
