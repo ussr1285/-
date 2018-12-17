5b0b4decbdf816310db084863cccb200a85503e6
Minjae Joe <ussr1285@gmail.com>
2018-12-17 20:24:00 +0900

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -53,11 +53,11 @@ public class Main {
                 temp_string[i] = total_text.substring(index_num[i]);
             }
             //System.out.println(temp_string[i]);
-            index_select[i][0] = temp_string[i].indexOf("①");
-            index_select[i][1] = temp_string[i].indexOf("②");
-            index_select[i][2] = temp_string[i].indexOf("③");
-            index_select[i][3] = temp_string[i].indexOf("④");
-            index_select[i][4] = temp_string[i].indexOf("⑤");
+            index_select[i][0] = temp_string[i].indexOf("① ");
+            index_select[i][1] = temp_string[i].indexOf("② ");
+            index_select[i][2] = temp_string[i].indexOf("③ ");
+            index_select[i][3] = temp_string[i].indexOf("④ ");
+            index_select[i][4] = temp_string[i].indexOf("⑤ ");
 
             int temp_index_dot;
             int dot_mark;
@@ -95,9 +95,11 @@ public class Main {
                 if(j < 4){ // 5 - 1 = 4
                     //System.out.println("----------------"+index_select[i][j]);
                     problem[i].get_select(j, temp_string[i].substring(index_select[i][j]+1, index_select[i][j+1])); //번호 제외를 위해 +1
+                    //problem[i].get_select(j, temp_string[i].substring(index_select[i][j], index_select[i][j+1]));
                 }
                 else{
                     problem[i].get_select(j, temp_string[i].substring(index_select[i][j]+1)); //동일
+                    //problem[i].get_select(j, temp_string[i].substring(index_select[i][j]));
                 }
             }
             System.out.println("--------second "+i+"st is not problem. -----------");
@@ -106,6 +108,7 @@ public class Main {
 
         System.out.println("정답이 담긴 파일 경로를 입력해 주십시오. ( ex. C:\\correct.txt )"); // 정답 수집기
         String file_correct_address = sc.next(); //"D:\\correct.txt";
+        //String file_correct_address = file_input_address.substring(0,15)+"_correct"+".txt";
         String total_correct_text;
         StringBuilder temp_correct_text = new StringBuilder();
         int correct_num[] = new int[amount_problem];
@@ -142,6 +145,64 @@ public class Main {
             }
             problem[i].get_correct(temp_correct);
         }
+
+/*
+        System.out.println("해설이 담긴 파일 경로를 입력해 주십시오. ( ex. C:\\hsj.txt )"); // 해설 수집기
+        String file_hsj_address = sc.next(); //"D:\\hsj.txt";
+        //String file_hsj_address =  file_input_address.substring(0,5)+"hsj"+file_input_address.substring(8);
+        String total_hsj_text;
+        String temp_hsj_string[] = new String[amount_problem];
+        int hsj_index_question_dot[] = new int[amount_problem];
+        int hsj_index_num[] = new int[amount_problem];
+        int hsj_index_select[][] = new int[amount_problem][5];
+
+        StringBuilder temp_hsj_text = new StringBuilder();
+        BufferedReader br3 = new BufferedReader(new FileReader(file_hsj_address));
+        while(true) {
+            String line = br3.readLine();
+            if (line==null) break;
+            temp_hsj_text.append(line);
+        }
+        br3.close();
+
+        total_hsj_text = temp_hsj_text.toString();
+        for (int i=0; i<amount_problem; i++) {
+            hsj_index_num[i] = total_hsj_text.indexOf(Integer.toString(i + 1)+".");
+        }
+        for (int i=0; i<amount_problem; i++){
+            System.out.println(i+"start------------------");
+            if(i < amount_problem-1){
+                temp_hsj_string[i] = total_hsj_text.substring(hsj_index_num[i], hsj_index_num[i+1]);
+            }
+            else{
+                temp_hsj_string[i] = total_hsj_text.substring(hsj_index_num[i]);
+            }
+
+            int temp_index_dot;
+            int dot_mark;
+            int question_mark;
+
+            if(temp_hsj_string[i].substring(3).indexOf("?") != -1){ question_mark = temp_hsj_string[i].substring(3).indexOf("?"); }
+            else{ question_mark = 999999999; }
+            if(temp_hsj_string[i].substring(3).indexOf(".") != -1){ dot_mark = temp_hsj_string[i].substring(3).indexOf("."); }
+            else{ dot_mark = 999999999; }
+
+            if (dot_mark < question_mark){
+                temp_index_dot = dot_mark;
+            }
+            else {
+                temp_index_dot = question_mark;
+            }
+            if (temp_index_dot != -1){
+                hsj_index_question_dot[i] = temp_index_dot+3; // 바로 위의 변수 초기화에서 2부터 시작하는 문장에서의 .을 찾으므로 +2를 해주어야 원래 위치.
+            }
+            System.out.println(i+"end------------------");
+        }
+        for(int i=0; i<amount_problem; i++) {
+            problem[i].get_hsj(temp_hsj_string[i].substring(0,hsj_index_question_dot[i])+"<br>"+temp_hsj_string[i].substring(hsj_index_question_dot[i]+1));
+        }
+*/
+
         System.out.println("추출할 모의고사 문제의 년도를 입력해주세요 : ");
         int year =  sc.nextInt();
         System.out.println("추출할 모의고사 문제의 월을 입력해주세요 : ");
@@ -154,7 +215,7 @@ public class Main {
 
         for (int i=0; i<amount_problem;i++) {
             problem[i].print_info();
-            problem[i].print_file(year, month, subject, grade);
+            problem[i].print_file(i, year, month, subject, grade);
         }
 
         System.out.println("분리완료");
